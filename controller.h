a6eeb5487ebf04754dc36af0b5a8bf3bf29bf0c9
paulzfm <zhufengminpaul@163.com>
2015-07-08 21:05:06 +0800

    basic framework

---
+++ controller.h
@@ -0,0 +1,18 @@
+#ifndef CONTROLLER_H
+#define CONTROLLER_H
+
+#include <QWidget>
+
+class Controller : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit Controller(QWidget *parent = 0);
+
+signals:
+
+public slots:
+
+};
+
+#endif // CONTROLLER_H
